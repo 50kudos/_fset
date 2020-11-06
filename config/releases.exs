c9118a6972ef6b357a0a42914058b31ab0a8aa90
50kudos <50th.zaint@gmail.com>
2020-11-06 20:55:45 +0700

    Connect db via pgBouncer

--- config/releases.exs
+++ config/releases.exs
@@ -14,7 +14,8 @@ database_url =
 config :fset, Fset.Repo,
   ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "20"),
+  prepare: :unnamed
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
